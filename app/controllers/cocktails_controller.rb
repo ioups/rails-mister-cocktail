57995bbef488a957ec6ab5048f4e56d1d66b8912
Pierre-Emmanuel Daigre <pe.daigre@gmail.com>
2021-02-12 00:11:09 +0100

    getting solution from wagon

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,24 +5,25 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
-    @cocktail = cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'new'
     end
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredient, :dose)
+    params.require(:cocktail).permit(:name)
   end
 end
